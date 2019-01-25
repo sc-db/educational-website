7d1b7db2fa39f30946d64a56bf2f8fb99a554759
Debanik <debanik@live.com>
2019-01-25 11:12:27 +0530

    final commit

--- course/models.py
+++ course/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to = 'images/')
-    text = models.TextField()
+    text = RichTextField()
 
     def __str__(self):
         return self.title
--- edu/settings.py
+++ edu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ckeditor',
     'allcourses.apps.AllcoursesConfig',
     'course.apps.CourseConfig',
     'django.contrib.admin',
